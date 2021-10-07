384b81a27afa3a2c32156befabdada2ae84e4067
amit singh <amritsinghh777@gmail.com>
2021-10-08 04:24:17 +0530

    intital laaravel 8 + page integrated

---
+++ resources/views/wap.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+
+<head>
+    <meta charset=utf-8>
+    <meta name=viewport content="width=device-width, initial-scale=1">
+    <meta name=keywords content="">
+    <meta name=description content="Whatsapp Uprint.id, customer service kami siap untuk membantu Anda">
+    <title>Uprint.id - Kontak Whatsapp</title>
+    <link rel="stylesheet" href="{{asset('css/styles.css')}}">
+    <link rel="icon" type=image/x-icon href="{{asset('media/favicon/stores/1/Uprint_id-Percetakan-Online-Indonesia.png')}}">
+    <script async src=http://www.googletagmanager.com/gtag/js?id=UA-76630295-1></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+        gtag('config', 'UA-76630295-1');
+    </script>
+    <style>
+        .page_speed_168804105 {
+            background-size: 100%
+        }
+
+        .page_speed_94614087 {
+            border-radius: 50%
+        }
+
+        .page_speed_1298542476 {
+            font-weight: bold
+        }
+
+        .page_speed_867482474 {
+            color: #ffc211
+        }
+    </style>
+</head>
+<style>
+    html {
+        background-color: #64656a;
+    }
+</style>
+
+<body class="text-gray-100 text-sm font-sans page_speed_168804105">
+    <div class="flex justify-center">
+        <div class="mt-6 mb-3"><img src="{{asset('images/logouprint.jpg')}}" alt="Uprint.id - Kontak Whatsapp Percetakan Online Indonesia" width=96 height=96 class="page_speed_94614087"></div>
+    </div>
+    <div class="flex justify-center">
+        <div class="border-white border-opacity-100 mb-3 font-bold"> Whatsapp Channel @uprint.id </div>
+    </div>
+    <div class="flex justify-center">
+        <div class="w-4/5 md:w-1/2 lg:w-1/2 py-2 my-2 text-center"> Halo Kak.. agar cs kami dapat melayani <span class="page_speed_1298542476">whatsapp</span> kakak dengan cepat, kakak bisa isi <span class="page_speed_1298542476">nama dan kebutuhan</span> lalu klik tombol dibawah ya kak </div>
+    </div>
+</body>
+
+</html>
